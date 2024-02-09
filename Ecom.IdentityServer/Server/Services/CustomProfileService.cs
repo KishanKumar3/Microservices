6ba96e8e4728f3dd977fe997aeaa1a01d55b1cbf
Kishan Kumar <kishan.kumar01@nagarro.com>
2024-02-09 17:49:43 +0530

    Is4

---
+++ Ecom.IdentityServer/Server/Services/CustomProfileService.cs
@@ -0,0 +1,35 @@
+﻿using IdentityModel;
+using IdentityServer4.Extensions;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace Server.Services
+{
+    public class CustomProfileService : IProfileService
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public CustomProfileService(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public  async Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            var user = await _userManager.GetUserAsync(context.Subject);
+            var claims = new List<Claim>
+        { //adding user-id claim
+            new Claim("user-id", user.Id)
+        };
+            context.IssuedClaims.AddRange(claims);
+            
+        }
+
+        public Task IsActiveAsync(IsActiveContext context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
